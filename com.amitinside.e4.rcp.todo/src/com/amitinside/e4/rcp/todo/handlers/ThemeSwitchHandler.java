9dcee9d6a0e843bffb330f65825477d34f4e62a9
Amit Kumar Mondal <admin@amitinside.com>
2015-03-17 23:42:13 +0100

    Added Workaround for IThemeEngine injection in case we use Jeeeyul's Themes

--- com.amitinside.e4.rcp.todo/src/com/amitinside/e4/rcp/todo/handlers/ThemeSwitchHandler.java
+++ com.amitinside.e4.rcp.todo/src/com/amitinside/e4/rcp/todo/handlers/ThemeSwitchHandler.java
@@ -2,6 +2,8 @@ package com.amitinside.e4.rcp.todo.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
+import org.eclipse.e4.ui.css.swt.theme.IThemeManager;
+import org.eclipse.swt.widgets.Display;
 
 public class ThemeSwitchHandler {
 	private static final String DEFAULT_THEME = "com.amitinside.e4.rcp.todo.default";
@@ -10,15 +12,20 @@ public class ThemeSwitchHandler {
 	// Remember the state
 	static boolean defaulttheme = true;
 
+	// IThemeEngine can not be injected in case we use Jeeeyul CSS from
+	// applicationCSS property (product extension point)
+	// As a workaround, we can inject IThemeManager to get IThemeEngine and then
+	// we can use it to set new theme dynamically
+
 	@Execute
-	public void switchTheme(IThemeEngine engine) {
-		// second argument defines that change is
-		// persisted and restored on restart
-		System.out.println("################"+engine.getActiveTheme());
+	public void switchTheme(IThemeManager manager) {
+		final IThemeEngine engine = manager.getEngineForDisplay(Display
+				.getCurrent());
+		engine.setTheme(RAINBOW_THEME, true);
 		if (!engine.getActiveTheme().getId().equals(DEFAULT_THEME)) {
 			engine.setTheme(DEFAULT_THEME, true);
 		} else {
-			
+
 			engine.setTheme(RAINBOW_THEME, true);
 		}
 	}
